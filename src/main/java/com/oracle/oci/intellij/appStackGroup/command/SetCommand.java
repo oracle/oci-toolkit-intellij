1b5ed34fefcb017c45324a9e95a8f433b2b5169e
Cameron Bateman <cameron.bateman@oracle.com>
2023-10-31 02:18:53 -0700

    Try merge again.

---
+++ src/main/java/com/oracle/oci/intellij/appStackGroup/command/SetCommand.java
@@ -0,0 +1,78 @@
+package com.oracle.oci.intellij.appStackGroup.command;
+
+import java.beans.PropertyDescriptor;
+
+import com.oracle.oci.intellij.common.Utils;
+import com.oracle.oci.intellij.common.command.AbstractBasicCommand;
+import com.oracle.oci.intellij.common.command.AbstractBasicCommand.Result.Severity;
+import com.oracle.oci.intellij.common.command.AbstractBasicCommand.Result.Status;
+
+public class SetCommand<ModelType, ObjType> extends AbstractBasicCommand {
+	public static class SetCommandResult<ModelType, ObjType> extends Result {
+		private ModelType modelType;
+		private ObjType oldValue;
+		private ObjType newValue;
+
+		public SetCommandResult(Severity severity, Status status, String message) {
+			super(severity, status, message);
+		}
+
+		public SetCommandResult(Severity error, Status failed, Throwable t) {
+			super(error, failed, t);
+		}
+
+		public SetCommandResult(Severity severity, Status status) {
+			super(severity, status);
+		}
+
+		public ModelType getModelType() {
+			return modelType;
+		}
+
+		public void setModelType(ModelType modelType) {
+			this.modelType = modelType;
+		}
+
+		public ObjType getObjType() {
+			return oldValue;
+		}
+
+		public void setOldValue(ObjType objType) {
+			this.oldValue = objType;
+		}
+
+		public ObjType getOldValue() {
+			return oldValue;
+		}
+
+		public void setNewValue(ObjType newValue) {
+			this.newValue = newValue;
+		}
+
+		public ObjType getNewValue() {
+			return newValue;
+		}
+	}
+
+	private ModelType modelObj;
+	private PropertyDescriptor pd;
+	private ObjType newValue;
+	private ObjType oldValue;
+
+	public SetCommand(ModelType modelObj, PropertyDescriptor pd, ObjType newValue) {
+		this.modelObj = modelObj;
+		this.pd = pd;
+		this.newValue = newValue;
+	}
+
+	@Override
+	protected SetCommand.SetCommandResult doExecute() throws Exception {
+		Object propValue =  Utils.setPropertyValue(modelObj, pd, newValue);
+		this.oldValue = (ObjType) propValue;
+		SetCommand.SetCommandResult<ModelType, ObjType> result = new SetCommandResult(Severity.NONE, Status.OK);
+		result.setModelType(modelObj);
+		result.setOldValue(oldValue);
+		result.setNewValue(newValue);
+		return result;
+	}
+}
---
+++ src/main/java/com/oracle/oci/intellij/common/command/AbstractBasicCommand.java
@@ -0,0 +1,107 @@
+package com.oracle.oci.intellij.common.command;
+
+public abstract class AbstractBasicCommand {
+	public static class CommandFailedException extends Exception {
+
+		private static final long serialVersionUID = -827559900110505792L;
+
+		public CommandFailedException(String message, Throwable cause) {
+			super(message, cause);
+		}
+
+		public CommandFailedException(Throwable cause) {
+			super(cause);
+		}
+	}
+	
+	public static class Result {
+		public enum Severity {
+			NONE, INFO, WARNING, ERROR;
+		}
+
+		private final Result.Severity severity;
+
+		public enum Status {
+			OK, CANCELLED, FAILED;
+		}
+
+		private final Result.Status status;
+		private final String message;
+		private Throwable exception;
+
+		public static final AbstractBasicCommand.Result OK_RESULT = new Result(Severity.NONE, Status.OK);
+
+		public Result(Result.Severity severity, Result.Status status) {
+			this(severity, status, (String) null);
+		}
+
+		public boolean isOk() {
+			return status == Status.OK;
+		}
+
+		public static AbstractBasicCommand.Result exception(Throwable t) {
+			return new Result(Severity.ERROR, Status.FAILED, t);
+		}
+
+		public Result(Result.Severity severity, Result.Status status, String message) {
+			this.severity = severity;
+			this.status = status;
+			this.message = message;
+		}
+
+		public Result(Result.Severity error, Result.Status failed, Throwable t) {
+			this(error, failed, t.getMessage());
+			this.exception = t;
+		}
+
+		public Result.Severity getSeverity() {
+			return severity;
+		}
+
+		public Result.Status getStatus() {
+			return status;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@SuppressWarnings("unused")
+		public Throwable getException() {
+			return exception;
+		}
+
+		public String toString() {
+			return String.format("Result: %s[%s] %s", getStatus(), getSeverity(),
+					getMessage() == null ? "" : getMessage());
+		}
+	}
+
+	public final AbstractBasicCommand.Result execute() throws Exception
+	{
+		try {
+			return doExecute();
+		}
+		catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	private boolean hasExecuted;
+	
+	protected abstract Result doExecute() throws Exception;
+
+	public boolean hasExecuted( ) {
+		return this.hasExecuted;
+	}
+	public boolean canUndo() {
+		return false;
+	}
+	
+	public void undo() {
+		// do nothing
+		if (!canUndo()) {
+			throw new IllegalStateException();
+		}
+	}
+}
