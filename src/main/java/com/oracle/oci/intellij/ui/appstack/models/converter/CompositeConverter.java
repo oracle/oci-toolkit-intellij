9877750d402ca255c3a8b9c71dfb200dafe98eab
Cameron Bateman <cameron.bateman@oracle.com>
2023-11-03 03:16:11 -0700

    Start converters, add test data and appstack template zip.

---
+++ src/main/java/com/oracle/oci/intellij/ui/appstack/models/converter/CompositeConverter.java
@@ -0,0 +1,43 @@
+package com.oracle.oci.intellij.ui.appstack.models.converter;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompositeConverter<T extends Object> extends Converter<T> {
+  public final static CompositeConverter<Object> DEFAULT_CONVERTERS;
+  static {
+    Map<Class<?>, Converter<?>> converters = new HashMap<>();
+    converters.put(Integer.class, new NumberConverter());
+    converters.put(Enum.class, new EnumConverter());
+    
+    DEFAULT_CONVERTERS = (CompositeConverter<Object>) Collections.unmodifiableMap(converters);
+  }
+
+  private Map<Class<?>, Converter<Object>> converters;
+
+  public CompositeConverter(Map<Class<?>, Converter<Object>> converters) {
+    this.converters = new HashMap<>();
+  }
+
+  @Override
+  public T from(String val, Class<T> clazz) throws ConversionException {
+    @SuppressWarnings("unchecked")
+    Converter<T> converter = (Converter<T>) this.converters.get(clazz);
+    if (converter != null) {
+      return converter.from(val, clazz);
+    }
+    throw new ConversionException("No converter for class: "+clazz.toString());
+  }
+
+  @Override
+  public String to(T val, Class<T> clazz) throws ConversionException {
+    @SuppressWarnings("unchecked")
+    Converter<T> converter = (Converter<T>) this.converters.get(clazz);
+    if (converter != null) 
+    {
+      return converter.to(val, clazz);
+    }
+    throw new ConversionException("No converter for class: "+clazz.toString());
+  }
+}
