5b3a8bee14b816bf6efcb3fdca70e36a488786c4
ayoub allali <ayoub.allali@oracle.com>
2023-11-14 15:21:43 +0100

    complete visible dependency

--- src/main/java/com/oracle/oci/intellij/ui/appstack/actions/AppStackParametersDialog.java
+++ src/main/java/com/oracle/oci/intellij/ui/appstack/actions/AppStackParametersDialog.java
@@ -1,18 +1,14 @@
 package com.oracle.oci.intellij.ui.appstack.actions;
 
 
-import com.intellij.ide.wizard.AbstractWizard;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.ValidationInfo;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.*;
 import com.intellij.ui.wizard.WizardDialog;
 import com.intellij.ui.wizard.WizardModel;
 import com.intellij.ui.wizard.WizardNavigationState;
 import com.intellij.ui.wizard.WizardStep;
-import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.JBDimension;
 import com.oracle.bmc.core.model.Subnet;
 import com.oracle.bmc.core.model.Vcn;
 import com.oracle.bmc.database.model.AutonomousDatabaseSummary;
@@ -24,8 +20,8 @@ import com.oracle.bmc.keymanagement.model.VaultSummary;
 import com.oracle.oci.intellij.account.OracleCloudAccount;
 import com.oracle.oci.intellij.ui.appstack.models.VariableGroup;
 import com.oracle.oci.intellij.ui.common.CompartmentSelection;
+import jnr.a64asm.LABEL_STATE;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -49,6 +45,9 @@ public class AppStackParametersDialog  {
     List <JLabel> errorLabels = new ArrayList<>() ;
 
    static Map<String, VariableGroup> variableGroups;
+
+   // this for each variable panel
+   LinkedHashMap <String, JComponent> varPanels= new LinkedHashMap<>();
    WizardModel wizardModel ;
 
 
@@ -126,10 +125,10 @@ public class AppStackParametersDialog  {
         JLabel label = new JLabel( pd.getDisplayName());
         label.setToolTipText( pd.getShortDescription());
         JPanel varPanel = new JPanel(new BorderLayout());
-        JPanel componentErrorPan = new JPanel();
-        componentErrorPan.setLayout(new BoxLayout(componentErrorPan, BoxLayout.Y_AXIS));
+        JPanel componentErrorPanel = new JPanel();
+        componentErrorPanel.setLayout(new BoxLayout(componentErrorPanel, BoxLayout.Y_AXIS));
 //        componentErrorPan.setBorder(new DottedBorder(Color.pink));
-        componentErrorPan.setPreferredSize(new Dimension(300,40));
+        componentErrorPanel.setPreferredSize(new Dimension(300,40));
 
 //        varPanel.setBorder(new DottedBorder(Color.pink));
         JComponent component ;
@@ -151,12 +150,14 @@ public class AppStackParametersDialog  {
         component = createComponentVariable(pd, varGroup,errorLabel);
 
 
-        componentErrorPan.add(component);
-        componentErrorPan.add(errorLabel,BorderLayout.CENTER);
+        componentErrorPanel.add(component);
+        componentErrorPanel.add(errorLabel,BorderLayout.CENTER);
         varPanel.add(label,BorderLayout.WEST);
-        varPanel.add(componentErrorPan,BorderLayout.EAST);
+        varPanel.add(componentErrorPanel,BorderLayout.EAST);
+        varPanels.put(pd.getName(),varPanel);
         // visibility
         boolean  isVisible = isVisible((String) pd.getValue("visible"));
+//        varPanel.hide();
         varPanel.setVisible(isVisible);
         return varPanel;
     }
@@ -165,7 +166,6 @@ public class AppStackParametersDialog  {
 
         Class<?> propertyType = pd.getPropertyType();
         JComponent component ;
-
 
         if (propertyType.getName().equals("boolean")) {
 
@@ -176,6 +176,8 @@ public class AppStackParametersDialog  {
                 try {
                     pd.setValue("value",checkBox.isSelected());
                     pd.getWriteMethod().invoke(varGroup,checkBox.isSelected());
+
+                    updateVisibility(pd);
                 } catch (IllegalAccessException | InvocationTargetException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -299,6 +301,7 @@ public class AppStackParametersDialog  {
                             } catch (IllegalAccessException | InvocationTargetException ex) {
                                 throw new RuntimeException(ex);
                             }
+                            updateVisibility(pd);
                             updateDependencies(pd, varGroup);
                         }
 
@@ -385,8 +388,21 @@ public class AppStackParametersDialog  {
         pdComponents.put(pd.getName(),component);
         component.setPreferredSize(new Dimension(200,30));
 
+        component.setBorder(new SideBorder(Colors.DARK_RED,1));
 
         return component;
+    }
+
+    void updateVisibility(PropertyDescriptor pd){
+        List<String> dependencies = Utils.visibilty.get(pd.getName());
+        if (dependencies != null) {
+
+            for (String dependency : dependencies) {
+                JComponent varPanel = varPanels.get(dependency);
+                PropertyDescriptor dependentPd = descriptorsState.get(dependency);
+                varPanel.setVisible(isVisible((String) dependentPd.getValue("visible")));
+            }
+        }
     }
     void updateDependencies(PropertyDescriptor pd, VariableGroup varGroup){
 
@@ -629,6 +645,26 @@ class Utils{
         put("vault_id",List.of("key_id"));
         put("vcn_compartment_id",List.of("existing_vcn_id","existing_app_subnet_id","existing_db_subnet_id","existing_lb_subnet_id"));
         put("existing_vcn_id",List.of("existing_app_subnet_id","existing_db_subnet_id","existing_lb_subnet_id"));
+    }};
+
+    static public Map<String, List<String>> visibilty = new LinkedHashMap<>() {{
+        put("application_source", List.of("application_type", "repo_name", "branch", "build_command", "artifact_location", "artifact_id", "registry_id", "image_path", "exposed_port", "use_username_env", "use_password_env", "use_tns_admin_env", "tns_admin_env", "use_default_ssl_configuration", "cert_pem", "private_key_pem", "ca_pem", "vm_options", "program_arguments"));
+        put("application_type", List.of("program_arguments", "use_default_ssl_configuration"));
+        put("use_existing_database", List.of("autonomous_database_display_name", "autonomous_database_admin_password", "data_storage_size_in_tbs", "cpu_core_count", "ocpu_count", "autonomous_database", "autonomous_database_user", "autonomous_database_password", "use_existing_db_subnet", "db_subnet_cidr"));
+        put("use_existing_vault", List.of("new_vault_display_name", "vault_compartment_id", "vault_id", "key_id"));
+        put("use_existing_token", List.of("current_user_token"));
+        put("use_connection_url_env", List.of("connection_url_env"));
+        put("use_username_env", List.of("username_env"));
+        put("use_password_env", List.of("password_env"));
+        put("use_tns_admin_env", List.of("tns_admin_env"));
+        put("use_default_ssl_configuration", List.of("port_property", "keystore_property", "key_alias_property", "keystore_password_property", "keystore_type_property"));
+        put("create_fqdn", List.of("dns_compartment", "zone", "subdomain", "certificate_ocid"));
+        put("create_new_vcn", List.of("vcn_compartment_id", "existing_vcn_id", "vcn_cidr", "use_existing_app_subnet", "use_existing_db_subnet", "use_existing_lb_subnet"));
+        put("use_existing_app_subnet", List.of("existing_app_subnet_id", "app_subnet_cidr"));
+        put("use_existing_db_subnet", List.of("existing_db_subnet_id", "db_subnet_cidr"));
+        put("use_existing_lb_subnet", List.of("existing_lb_subnet_id", "lb_subnet_cidr"));
+        put("use_default_lb_configuration", List.of("maximum_bandwidth_in_mbps", "minimum_bandwidth_in_mbps", "health_checker_url_path", "health_checker_return_code", "enable_session_affinity"));
+        put("enable_session_affinity", List.of("session_affinity", "session_affinity_cookie_name"));
     }};
 
     static SuggestConsumor<PropertyDescriptor,LinkedHashMap<String,PropertyDescriptor> ,List<? extends ExplicitlySetBmcModel>,VariableGroup> getSuggestedValuesOf(String type){
